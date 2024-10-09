be12937cbc624058ffb2d6522c542605fbfae7b1
harshu833 <harshithashivaraj23@gmail.com>
2024-10-09 17:32:00 +0530

    java

---
+++ MethodOverloading.java
@@ -0,0 +1,58 @@
+public class MethodOverloading {
+    public int sum(int a, int b) {
+        return a + b;
+    }
+    public double sum(double a, double b) {
+        return a + b;
+    }
+    public String sum(String a, String b) {
+        return a + b;
+    }
+
+    public void print(int a) {
+        System.out.println("Integer: " + a);
+    }
+    public void print(String b) {
+        System.out.println("String: " + b);
+    }
+
+    public void print(double c) {
+        System.out.println("Double: " + c);
+    }
+
+    public double area(double radius) {
+        return Math.PI * radius * radius; 
+    }
+    public double area(double length, double width) {
+        return length * width; 
+    }
+    public double area(double base, double height, boolean isTriangle) {
+        return (base * height) / 2; 
+    }
+
+    public int sumVarArgs(int... numbers) {
+        int sum = 0; 
+        for (int num : numbers) {
+            sum += num;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+    
+        MethodOverloading  example = new MethodOverloading();
+
+        System.out.println("Sum of integers: " + example.sum(10, 20)); 
+        System.out.println("Sum of doubles: " + example.sum(10.5, 20.5)); 
+        System.out.println("Sum of strings: " + example.sum("Hello, ", "World!")); 
+
+        example.print(100); 
+        example.print("Hello"); 
+        example.print(99.99); 
+
+        System.out.println("Area of Circle: " + example.area(5.0)); 
+        System.out.println("Area of Rectangle: " + example.area(5.0, 10.0)); 
+        System.out.println("Area of Triangle: " + example.area(5.0, 10.0, true)); 
+        System.out.println("Sum of variable arguments: " + example.sumVarArgs(1, 2, 3, 4, 5)); 
+    }
+}
