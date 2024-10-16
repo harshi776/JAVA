18720cdba882c238049153c74042be704a2fc7ab
harshu833 <harshithashivaraj23@gmail.com>
2024-10-16 19:22:20 +0530

    State and Behaviour

---
+++ Transport.java
@@ -0,0 +1,22 @@
+public class Transport {
+    String transportType;
+    int transportNumber;
+    String destination;
+    int capacity;
+    int currentPassengers;
+
+    public static void main(String[] args) {
+    }
+
+    public static void start() {
+    }
+
+    public static void stop() {
+    }
+
+    public static void changeDestination() {
+    }
+
+    public static void announcement() {
+    }
+}
