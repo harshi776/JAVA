ab2779e04f90d7dcdf2eab873a023e56f26b1838
harshu833 <harshithashivaraj23@gmail.com>
2024-10-16 19:24:23 +0530

    Objects and Class

---
+++ TransportInfo.java
@@ -0,0 +1,148 @@
+public class TransportInfo {
+    int transportNumber;
+    String destination;
+    int capacity;
+    String type;
+    int currentPassengers;
+
+    public static void main(String[] args) {
+        Transport transport = new Transport();
+        transport.transportNumber = 1233;
+        transport.destination = "Bengaluru";
+        transport.capacity = 2000;
+        transport.transportType = "Express";
+        transport.currentPassengers = 1200;
+
+        System.out.println("transportNumber: " + transport.transportNumber);
+        System.out.println("destination: " + transport.destination);
+        System.out.println("capacity: " + transport.capacity);
+        System.out.println("transportType: " + transport.transportType);
+        System.out.println("currentPassengers: " + transport.currentPassengers);
+
+        System.out.println("-----------------------------------");
+
+        Transport transport1 = new Transport();
+        transport1.transportNumber = 1127;
+        transport1.destination = "Mysore";
+        transport1.capacity = 3000;
+        transport1.transportType = "Express";
+        transport1.currentPassengers = 1100;
+        System.out.println("transportNumber: " + transport1.transportNumber);
+        System.out.println("destination: " + transport1.destination);
+        System.out.println("capacity: " + transport1.capacity);
+        System.out.println("transportType: " + transport1.transportType);
+        System.out.println("currentPassengers: " + transport1.currentPassengers);
+
+        System.out.println("-----------------------------------");
+
+        Transport transport2 = new Transport();
+        transport2.transportNumber = 14245;
+        transport2.destination = "Chennai";
+        transport2.capacity = 4000;
+        transport2.transportType = "Ordinary";
+        transport2.currentPassengers = 1500;
+        System.out.println("transportNumber: " + transport2.transportNumber);
+        System.out.println("destination: " + transport2.destination);
+        System.out.println("capacity: " + transport2.capacity);
+        System.out.println("transportType: " + transport2.transportType);
+        System.out.println("currentPassengers: " + transport2.currentPassengers);
+        
+        System.out.println("-----------------------------------");
+
+        Transport transport3 = new Transport();
+        transport3.transportNumber = 2574;
+        transport3.destination = "New Delhi";
+        transport3.capacity = 4400;
+        transport3.transportType = "Ordinary";
+        transport3.currentPassengers = 1700;
+        System.out.println("transportNumber: " + transport3.transportNumber);
+        System.out.println("destination: " + transport3.destination);
+        System.out.println("capacity: " + transport3.capacity);
+        System.out.println("transportType: " + transport3.transportType);
+        System.out.println("currentPassengers: " + transport3.currentPassengers);
+
+        System.out.println("-----------------------------------");
+
+        Transport transport4 = new Transport();
+        transport4.transportNumber = 25632;
+        transport4.destination = "Ballari";
+        transport4.capacity = 2500;
+        transport4.transportType = "Ordinary";
+        transport4.currentPassengers = 1200;
+        System.out.println("transportNumber: " + transport4.transportNumber);
+        System.out.println("destination: " + transport4.destination);
+        System.out.println("capacity: " + transport4.capacity);
+        System.out.println("transportType: " + transport4.transportType);
+        System.out.println("currentPassengers: " + transport4.currentPassengers);
+        
+        System.out.println("-----------------------------------");
+
+        Transport transport5 = new Transport();
+        transport5.transportNumber = 6357;
+        transport5.destination = "Hubli";
+        transport5.capacity = 2800;
+        transport5.transportType = "Express";
+        transport5.currentPassengers = 1300;
+        System.out.println("transportNumber: " + transport5.transportNumber);
+        System.out.println("destination: " + transport5.destination);
+        System.out.println("capacity: " + transport5.capacity);
+        System.out.println("transportType: " + transport5.transportType);
+        System.out.println("currentPassengers: " + transport5.currentPassengers);
+
+        System.out.println("-----------------------------------");
+
+        Transport transport6 = new Transport();
+        transport6.transportNumber = 4427;
+        transport6.destination = "Hyderabad";
+        transport6.capacity = 4800;
+        transport6.transportType = "Express";
+        transport6.currentPassengers = 2500;
+        System.out.println("transportNumber: " + transport6.transportNumber);
+        System.out.println("destination: " + transport6.destination);
+        System.out.println("capacity: " + transport6.capacity);
+        System.out.println("transportType: " + transport6.transportType);
+        System.out.println("currentPassengers: " + transport6.currentPassengers);
+
+        System.out.println("-----------------------------------");
+
+        Transport transport7 = new Transport();
+        transport7.transportNumber = 5377;
+        transport7.destination = "Bombay";
+        transport7.capacity = 4500;
+        transport7.transportType = "Express";
+        transport7.currentPassengers = 3000;
+        System.out.println("transportNumber: " + transport7.transportNumber);
+        System.out.println("destination: " + transport7.destination);
+        System.out.println("capacity: " + transport7.capacity);
+        System.out.println("transportType: " + transport7.transportType);
+        System.out.println("currentPassengers: " + transport7.currentPassengers);
+
+        System.out.println("-----------------------------------");
+
+        Transport transport8 = new Transport();
+        transport8.transportNumber = 6357;
+        transport8.destination = "Mumbai";
+        transport8.capacity = 5000;
+        transport8.transportType = "Ordinary";
+        transport8.currentPassengers = 4000;
+        System.out.println("transportNumber: " + transport8.transportNumber);
+        System.out.println("destination: " + transport8.destination);
+        System.out.println("capacity: " + transport8.capacity);
+        System.out.println("transportType: " + transport8.transportType);
+        System.out.println("currentPassengers: " + transport8.currentPassengers);
+
+        System.out.println("-----------------------------------");
+
+        Transport transport9 = new Transport();
+        transport9.transportNumber = 7537;
+        transport9.destination = "Vijayawada";
+        transport9.capacity = 4500;
+        transport9.transportType = "Express";
+        transport9.currentPassengers = 3500;
+        System.out.println("transportNumber: " + transport9.transportNumber);
+        System.out.println("destination: " + transport9.destination);
+        System.out.println("capacity: " + transport9.capacity);
+        System.out.println("transportType: " + transport9.transportType);
+        System.out.println("currentPassengers: " + transport9.currentPassengers);
+    }
+}
